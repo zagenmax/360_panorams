dc514e30b69b6f0c29d3d2fec90bcf23358b63ae
zagenmax <150284835+zagenmax@users.noreply.github.com>
2023-11-08 22:20:12 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,71 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-room_1",
+      "name": "Room_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4069669810135341
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5514493591190952,
+          "pitch": 0.1409159083948488,
+          "rotation": 0,
+          "target": "1-room_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-room_2",
+      "name": "Room_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4069669810135341
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5907313448667075,
+          "pitch": 0.1265740926913832,
+          "rotation": 0,
+          "target": "0-room_1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
